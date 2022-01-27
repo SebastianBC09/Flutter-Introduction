a7f67594949bfa0e4933035f544d981a6848092f
Sebastian Ballen <sebastian.ballenc@gmail.com>
2022-01-26 21:59:13 -0500

    Configuration of routes for the new pages done in main file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/pages/avatar_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Componentes App',
-      home: HomePage(),
+      // home: HomePage(),
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext context) => const HomePage(),
+        '/Alert Page': (BuildContext context) => const AlertPage(),
+        '/Avatar Page': (BuildContext context) => const AvatarPage(),
+      },
     );
   }
 }
