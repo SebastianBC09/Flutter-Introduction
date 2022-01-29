320889fdbb29e126f15b8e448e4c0fda0dd36aae
Sebastian Ballen <sebastian.ballenc@gmail.com>
2022-01-28 23:29:23 -0500

    Task requested made: Creation of Card Page & making the routing connection for this new page.

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:componentes/src/pages/home_page.dart';
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
-import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/pages/card_page.dart';
 
 Map<String, WidgetBuilder> getApplicationsRoutes() {
   return <String, WidgetBuilder>{
     '/': (BuildContext context) => const HomePage(),
     'alert': (BuildContext context) => const AlertPage(),
     'avatar': (BuildContext context) => const AvatarPage(),
+    'card': (BuildContext context) => const CardPage(),
   };
 }
