1f9c9d7174fc3f473ea5fc040523ada3458bf6e6
Sebastian Ballen <sebastian.ballenc@gmail.com>
2022-01-28 23:49:24 -0500

    Creation of card type 1, this is the most basic type of card for flutter.

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -7,8 +7,42 @@ class CardPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Cards'),
+        title: const Text('Cards'),
         centerTitle: true,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(10.0),
+        children: [
+          _cardType1(),
+        ],
+      ),
+    );
+  }
+
+  _cardType1() {
+    return Card(
+      child: Column(
+        children: [
+          const ListTile(
+            leading: Icon(Icons.card_travel, color: Colors.blueGrey),
+            title: Text('Cart Type 1'),
+            subtitle: Text(
+                'Bienvenido a mi curso introductorio de Flutter en español, el cual tiene por objetivo enseñarte las bases sobre el lenguaje de Dart y el Flutter SDK.'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                onPressed: () {},
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text('Ok'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
