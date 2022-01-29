fc06f11124fd33b3f09b94fb6e3f119be473e5e8
Sebastian Ballen <sebastian.ballenc@gmail.com>
2022-01-28 23:24:55 -0500

    Creation of routes folder to manage the navigation between screens.

---
+++ lib/src/routes/routes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/pages/avatar_page.dart';
+import 'package:componentes/src/pages/home_page.dart';
+
+Map<String, WidgetBuilder> getApplicationsRoutes() {
+  return <String, WidgetBuilder>{
+    '/': (BuildContext context) => const HomePage(),
+    'alert': (BuildContext context) => const AlertPage(),
+    'avatar': (BuildContext context) => const AvatarPage(),
+  };
+}
