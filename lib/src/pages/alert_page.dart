2322e7aec311340874a364b410079ccf62c48bc3
Sebastian Ballen <sebastian.ballenc@gmail.com>
2022-02-01 14:56:58 -0500

    Minor styling changes added to page.

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -10,12 +10,57 @@ class AlertPage extends StatelessWidget {
         title: const Text('Alert Page'),
         centerTitle: true,
       ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Show Alert'),
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all(const StadiumBorder()),
+            backgroundColor:
+                MaterialStateProperty.all(Colors.indigoAccent.shade700),
+          ),
+          onPressed: () => _showAlert(context),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.arrow_back_ios_new_rounded),
         onPressed: () {
           Navigator.pop(context);
         },
       ),
+    );
+  }
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('🚨Alert Title🚨'),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0)),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text('This is the content of the alert box!'),
+              FlutterLogo(
+                size: 100.0,
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Ok'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
