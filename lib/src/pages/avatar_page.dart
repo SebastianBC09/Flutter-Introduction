2ad3689043853ee0772817bde6b19916f141e451
Sebastian Ballen <sebastian.ballenc@gmail.com>
2022-02-01 15:20:54 -0500

    Avatar Page finished. Two avatars added to the app bar, fade image too.

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -9,6 +9,32 @@ class AvatarPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Avatar Page'),
         centerTitle: true,
+        actions: [
+          const Padding(
+            padding: EdgeInsets.all(3.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://pbs.twimg.com/profile_images/1018943227791982592/URnaMrya_400x400.jpg'),
+              radius: 25.0,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: CircleAvatar(
+              radius: 25.0,
+              child: const Text('SL'),
+              backgroundColor: Colors.indigo.shade900,
+            ),
+          ),
+        ],
+      ),
+      body: const Center(
+        child: FadeInImage(
+          image: NetworkImage(
+              'https://www.cinconoticias.com/wp-content/uploads/Stan-Lee.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 250),
+        ),
       ),
     );
   }
